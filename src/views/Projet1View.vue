f2d934f10c9c18f6492dddf277a17eeb79acacef
ilkaymenn <ilkay.men@edu.univ-fcomte.fr>
2023-03-10 09:21:40 +0100

    projet 1 partie3 fini

--- src/views/Projet1View.vue
+++ src/views/Projet1View.vue
@@ -47,69 +47,90 @@
       <hr class="border-2 lg:w-80" />
     </div>
 
-    <div class="lg:mt-20">
-      <div class="lg:flex lg:justify-around">
-        <div>
-          <h2 class="lg:mb-5">achievement process</h2>
-          <div class="lg:flex">
-            <p class="font-iceberg lg:text-9xl">01</p>
-            <p class="lg:ml-10 lg:w-96">
-              I <strong class="font-iceberg">started</strong> by making a needs analysis of the site through a story mapping. Then I
-              modified the svg of the watch provided before in order to have a shape and a style that better suited my study. From there, I
-              was able to create the brandboard and the visual identity of the project (watch + site). From then on, I was able to start the
-              design of the website model on Figma.
-            </p>
+    <div>
+      <div class="lg:mt-20">
+        <div class="lg:flex lg:justify-around">
+          <div>
+            <h2 class="lg:mb-5">achievement process</h2>
+            <div class="lg:flex">
+              <p class="font-iceberg lg:text-9xl">01</p>
+              <p class="lg:ml-10 lg:w-96">
+                I <strong class="font-iceberg">started</strong> by making a needs analysis of the site through a story mapping. Then I
+                modified the svg of the watch provided before in order to have a shape and a style that better suited my study. From there,
+                I was able to create the brandboard and the visual identity of the project (watch + site). From then on, I was able to start
+                the design of the website model on Figma.
+              </p>
+            </div>
+          </div>
+          <div class="carousel lg:w-80">
+            <div class="carousel-item">
+              <img src="../assets/projet1/images/Face.png" alt="Tailwind CSS Carousel component" />
+            </div>
+            <div class="carousel-item ml-14">
+              <img src="../assets/projet1/images/ProfilRose.png" alt="Tailwind CSS Carousel component" />
+            </div>
+            <div class="carousel-item ml-10">
+              <img src="../assets/projet1/images/ProfilBlanc.png" alt="Tailwind CSS Carousel component" />
+            </div>
           </div>
         </div>
-        <div class="carousel lg:w-80">
-          <div class="carousel-item">
-            <img src="../assets/projet1/images/Face.png" alt="Tailwind CSS Carousel component" />
-          </div>
-          <div class="carousel-item ml-14">
-            <img src="../assets/projet1/images/ProfilRose.png" alt="Tailwind CSS Carousel component" />
-          </div>
-          <div class="carousel-item ml-10">
-            <img src="../assets/projet1/images/ProfilBlanc.png" alt="Tailwind CSS Carousel component" />
+      </div>
+
+      <div class="lg:grid lg:justify-items-center">
+        <img class="lg:mt-16 lg:w-3/4" src="../assets/projet1/images/Story Mapping.png" alt="" />
+      </div>
+
+      <div class="lg:mt-16 lg:grid lg:grid-cols-2 lg:items-center lg:justify-items-center">
+        <div class="lg:w-5/6"><img src="@/assets/projet1/images/Typographies.png" alt="" /></div>
+        <div class="lg:w-5/6"><img src="@/assets/projet1/images/HarmonieColoree.png" alt="" /></div>
+      </div>
+
+      <div class="lg:mt-16 lg:grid lg:grid-cols-2 lg:items-center lg:justify-items-center">
+        <div class="lg:grid lg:justify-items-center">
+          <div class="mb-5 lg:w-5/6"><img src="@/assets/projet1/images/RecherchesLogo.png" alt="" /></div>
+          <div class="lg:w-5/6"><img src="@/assets/projet1/images/LogoFinal.png" alt="" /></div>
+        </div>
+        <div class="lg:w-5/6"><img src="@/assets/projet1/images/Brandboard.png" alt="" /></div>
+      </div>
+
+      <div class="lg:mt-24 lg:grid lg:grid-cols-2 lg:items-center lg:justify-items-center">
+        <div class="lg:grid lg:justify-items-center">
+          <div class="mb-10"><img src="@/assets/projet1/images/pc1.png" alt="" /></div>
+          <div class=""><img src="@/assets/projet1/images/pc1.png" alt="" /></div>
+        </div>
+        <div class="lg:grid lg:justify-items-center">
+          <div class="mb-10"><img src="@/assets/projet1/images/pc1.png" alt="" /></div>
+          <div class=""><img src="@/assets/projet1/images/pc1.png" alt="" /></div>
+        </div>
+      </div>
+
+      <div class="lg:mt-20 lg:grid lg:justify-items-center">
+        <div class="lg:grid lg:justify-items-center">
+          <h2 class="">tools</h2>
+          <div class="flex lg:mt-4 lg:h-12">
+            <img src="@/assets/logo/design/Illustrator.png" alt="logo Illustrator" />
+            <img src="@/assets/logo/design/Figma.png" alt="logo Figma" />
           </div>
         </div>
       </div>
-    </div>
 
-    <div class="lg:grid lg:justify-items-center">
-      <img class="lg:mt-16 lg:w-3/4" src="../assets/projet1/images/Story Mapping.png" alt="" />
-    </div>
-
-    <div class="lg:mt-16 lg:grid lg:grid-cols-2 lg:items-center lg:justify-items-center">
-      <div class="lg:w-5/6"><img src="@/assets/projet1/images/Typographies.png" alt="" /></div>
-      <div class="lg:w-5/6"><img src="@/assets/projet1/images/HarmonieColoree.png" alt="" /></div>
-    </div>
-
-    <div class="lg:mt-16 lg:grid lg:grid-cols-2 lg:items-center lg:justify-items-center">
-      <div class="lg:grid lg:justify-items-center">
-        <div class="mb-5 lg:w-5/6"><img src="@/assets/projet1/images/RecherchesLogo.png" alt="" /></div>
-        <div class="lg:w-5/6"><img src="@/assets/projet1/images/LogoFinal.png" alt="" /></div>
+      <div class="lg:mt-16 lg:grid lg:justify-center">
+        <hr class="border-2 lg:w-80" />
       </div>
-      <div class="lg:w-5/6"><img src="@/assets/projet1/images/Brandboard.png" alt="" /></div>
-    </div>
 
-    <div class="lg:mt-24 lg:grid lg:grid-cols-2 lg:items-center lg:justify-items-center">
-      <div class="lg:grid lg:justify-items-center">
-        <div class="mb-10"><img src="@/assets/projet1/images/pc1.png" alt="" /></div>
-        <div class=""><img src="@/assets/projet1/images/pc1.png" alt="" /></div>
-      </div>
-      <div class="lg:grid lg:justify-items-center">
-        <div class="mb-10"><img src="@/assets/projet1/images/pc1.png" alt="" /></div>
-        <div class=""><img src="@/assets/projet1/images/pc1.png" alt="" /></div>
-      </div>
-    </div>
-
-    <div class="lg:mt-20 lg:grid lg:justify-items-center">
-      <div class="lg:grid lg:justify-items-center">
-        <h2 class="">tools</h2>
-        <div class="flex lg:mt-4 lg:h-12">
-          <img src="@/assets/logo/design/Illustrator.png" alt="logo Illustrator" />
-          <img src="@/assets/logo/design/Figma.png" alt="logo Figma" />
+      <div class="lg:mt-20 lg:grid">
+        <div class="lg:flex lg:items-center">
+          <p class="font-iceberg lg:text-9xl">02</p>
+          <p class="lg:ml-10 lg:w-96">
+            In the <strong class="font-iceberg">second</strong> phase, I carried out a marketing nudge study, including SMART objectives,
+            and the cognitive biases of this study.
+          </p>
         </div>
+      </div>
+
+      <div class="lg:mt-16 lg:grid lg:items-center lg:justify-items-center">
+        <div class="lg:w-4/6"><img src="@/assets/projet1/images/SMART.png" alt="image des objectifs SMART " /></div>
+        <div class="lg:mt-4 lg:w-4/6"><img src="@/assets/projet1/images/BiaisCognitifs.png" alt="image des biais cognitifs" /></div>
       </div>
     </div>
 
@@ -117,54 +138,39 @@
       <hr class="border-2 lg:w-80" />
     </div>
 
-    <div class="lg:mt-20 lg:grid">
-      <div class="lg:flex lg:items-center">
-        <p class="font-iceberg lg:text-9xl">02</p>
-        <p class="lg:ml-10 lg:w-96">
-          In the <strong class="font-iceberg">second</strong> phase, I carried out a marketing nudge study, including SMART objectives, and
-          the cognitive biases of this study.
-        </p>
-      </div>
-    </div>
-
-    <div class="lg:mt-16 lg:grid lg:items-center lg:justify-items-center">
-      <div class="lg:w-4/6"><img src="@/assets/projet1/images/SMART.png" alt="image des objectifs SMART " /></div>
-      <div class="lg:mt-4 lg:w-4/6"><img src="@/assets/projet1/images/BiaisCognitifs.png" alt="image des biais cognitifs" /></div>
-    </div>
-
-    <div class="lg:mt-16 lg:grid lg:justify-center">
-      <hr class="border-2 lg:w-80" />
-    </div>
-
-    <div class="lg:mt-20 lg:grid">
-      <div class="lg:flex lg:items-center">
-        <p class="font-iceberg lg:text-9xl">03</p>
-        <p class="lg:ml-10 lg:w-96">
-          So, in the <strong class="font-iceberg">third</strong> and last phase I developed my website with the Tailwind CSS Framework. And
-          I ended up putting it online on heroku.
-        </p>
-      </div>
-    </div>
-
-    <div class="lg:flex lg:justify-center">
-      <p class="lg:text-4xl">
-        You can access the website right
-        <a href="https://melodious-moonbeam-cf41df.netlify.app/"><strong class="font-iceberg uppercase underline">here</strong></a>
-      </p>
-    </div>
-
-    <div class="lg:mt-20 lg:grid lg:justify-items-center">
-      <div class="lg:grid lg:justify-items-center">
-        <h2 class="">tools</h2>
-        <div class="flex lg:mt-4 lg:h-12">
-          <img src="@/assets/logo/dev/VSC.png" alt="logo Illustrator" />
-          <img src="@/assets/logo/dev/TailwindCSS.png" alt="logo Figma" />
-          <img src="@/assets/logo/dev/VueJS.png" alt="logo Figma" />
-          <img src="@/assets/logo/dev/Heroku.png" alt="logo Figma" />
-          <img src="@/assets/logo/dev/Netlify.png" alt="logo Figma" />
-          <img src="@/assets/logo/dev/Github.png" alt="logo Figma" />
+    <div>
+      <div class="lg:mt-20 lg:grid">
+        <div class="lg:flex lg:items-center">
+          <p class="font-iceberg lg:text-9xl">03</p>
+          <p class="lg:ml-10 lg:w-96">
+            So, in the <strong class="font-iceberg">third</strong> and last phase I developed my website with the Tailwind CSS Framework.
+            And I ended up putting it online on heroku.
+          </p>
         </div>
       </div>
+
+      <div class="lg:flex lg:justify-center">
+        <p class="lg:text-4xl">
+          You can access the website right
+          <a href="https://melodious-moonbeam-cf41df.netlify.app/"><strong class="font-iceberg uppercase underline">here</strong></a>
+        </p>
+      </div>
+
+      <div class="lg:mt-20">
+        <h2 class="flex lg:justify-center">tools</h2>
+        <div class="flex lg:mt-4 lg:h-12 lg:justify-evenly">
+          <img src="@/assets/logo/dev/VSC.png" alt="logo Visual Studio Code" />
+          <img src="@/assets/logo/dev/TailwindCSS.png" alt="logo Tailwin CSS" />
+          <img src="@/assets/logo/dev/VueJS.png" alt="logo VueJS" />
+          <img src="../assets/logo/dev/Heroku.png" alt="logo Heroku" />
+          <img src="@/assets/logo/dev/Netlify.png" alt="logo Netlify" />
+          <img src="@/assets/logo/dev/Github.png" alt="logo Github" />
+        </div>
+      </div>
+    </div>
+
+    <div class="lg:mt-40 lg:grid lg:justify-center">
+      <hr class="w-80 border-2" />
     </div>
   </main>
 </template>
